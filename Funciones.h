670cf9be77abd0f913a797613f423d665b6a08fa
Sdlezcanoutn <sollezcanoutn@gmail.com>
2025-11-04 22:14:30 -0300

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -2,23 +2,23 @@
 #include <string>
 using namespace std;
 
-// Constantes base
+// CONSTANTESS
 const int CANT_BANCOS = 3;
 const int CANT_ANIOS  = 3;
 const int ANIO_BASE   = 2025;
 
-// Estructura básica
 struct Banco {
     string nombre;
     float  tasas[CANT_ANIOS];  // 3 tasas históricas
     float  promedio;           // promedio de las tasas
 };
 
-// Prototipos
+// DECLARACION DE FUNCIONES
 void cargarNombres(Banco bancos[]);
 void ingresarTasas(Banco bancos[]);
 void verPromedios(const Banco bancos[]);
 void verResultados(const Banco bancos[], float capital);
+
 
 
 
